0cb50d9c07b1401dc1a57fb9b8daa31edcfc8ef9
Richard Lynch <rlynch2012@gmail.com>
2024-01-29 00:05:33 -0500

    add button functionalityfor pic in pic

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const videoElement = document.getElementById('video');
 const button = document.getElementById('button');
-
+// const captureButton = document.getElementById('capture-button');
 // Prompt select media stream, pass to video element, then play
-async function selectMediaStream() {
+
+const selectMediaStream = async() => {
     try {
         const mediaStream = await navigator.mediaDevices.getDisplayMedia();
         videoElement.srcObject = mediaStream;
@@ -14,6 +15,17 @@ async function selectMediaStream() {
         console.log('whoops, error here:', error)
     }
 }
+
+// On Click Handler
+// captureButton.addEventListener('click', selectMediaStream);
+button.addEventListener('click', async () => {
+// Disable Button
+    button.disabled = true;
+    // Start Picture in Picture
+    await videoElement.requestPictureInPicture();
+    // Reset Button
+    button.disable = false;
+});
 
 //  On Load
 selectMediaStream();
